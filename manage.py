e88d52dfdb578c6a1005fd7669a9585bafed3133
pritika-rana <61936909+pritika-rana@users.noreply.github.com>
2020-08-03 15:50:20 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'police.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrimeDetection.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ weapon/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import weapon_model
+
+# Register your models here.
+
+admin.site.register(weapon_model)
